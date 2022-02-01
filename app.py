bbb1a7d4bbe5930ca329e795fc5da5fc84c60521
Sabina Khanal <sabina.khanal@cloudwick.com>
2022-02-01 16:46:06 +0000

    final push

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, request, jsonify
 from flask.logging import create_logger
-from flask.logging import logging
+import logging
 
 import pandas as pd
 #from sklearn.externals import joblib updating something
-from sklearn.externals import joblib
+import joblib
 from sklearn.preprocessing import StandardScaler
 
 app = Flask(__name__)
